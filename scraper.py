8419ad5ff8c0417514ba180da84305e693031980
Bhavya Bajaj <bhavyab1375@gmail.com>
2022-12-02 10:06:09 +0000

    Added the url of the webpage and stored on the local computer

--- scraper.py
+++ scraper.py
@@ -1,1 +1,6 @@
-print('Hello world')
+import requests
+UDEMY_DEVELOPMENT_COURSES_URL = 'https://www.udemy.com/courses/development/'
+response = requests.get(UDEMY_DEVELOPMENT_COURSES_URL)
+print('Status Code', response.status_code)
+with open('udemy-development.html', 'w') as f:
+  f.write(response.text)
